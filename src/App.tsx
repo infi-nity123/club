57e516f2138df52beceb8d84e12a536ee0e504cc
infi-nity123 <167573613+infi-nity123@users.noreply.github.com>
2025-10-01 20:01:07 +0000

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import Counter from "./component/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>hello</div>
+      <Counter />
     </>
   );
 }
---
+++ src/component/Counter.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+export default function Counter() {
+  const [count, setCount] = useState(() => {
+    const saved = localStorage.getItem("count");
+    return saved ? JSON.parse(saved) : 0;
+  });
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-[100vh]">
+        <div>{count}</div>
+        <div>
+          <button
+            onClick={() => setCount((prev) => prev - 1)}
+            className="bg-blue-700 text-white border rounded-md px-2 active:bg-blue-400"
+          >
+            Decrease
+          </button>
+          <button
+            onClick={() => setCount(0)}
+            className="bg-blue-700 text-white border rounded-md px-2 active:bg-blue-400"
+          >
+            Reset
+          </button>
+          <button
+            onClick={() => setCount((prev) => prev + 1)}
+            className="bg-blue-700 text-white border rounded-md px-2 active:bg-blue-400"
+          >
+            Increase
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
